6b2a705d63f494cf1c2b7532872f6bb8887f56eb
Eleazar Pin Etchave <eleazarpinetchave@gmail.com>
2019-05-18 16:59:55 -0300

    actualizado bat

--- generarExe.bat
+++ generarExe.bat
@@ -2,7 +2,7 @@ c:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c ts.c tercetos.c -o Primera.exe
 pause
 pause
 Primera.exe prueba.txt

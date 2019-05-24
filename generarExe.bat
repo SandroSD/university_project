23719dca1161a8060d2f3f160768f0b1567cfad3
SandroSD <sdezerio@gmail.com>
2019-05-24 00:03:59 -0300

    cambio de nombre del archivo de prueba

--- generarExe.bat
+++ generarExe.bat
@@ -5,7 +5,7 @@ pause
 c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c c_files\ts.c c_files\tercetos.c c_files\pila.c -o Primera.exe
 pause
 pause
-Primera.exe prueba_WHILE.txt
+Primera.exe prueba.txt
 del lex.yy.c
 del y.tab.c
 del y.output

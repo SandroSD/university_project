5c6832b9e56e3738953e89ce7968462ed25c4c87
Eleazar Pin Etchave <eleazarpinetchave@gmail.com>
2019-06-17 13:07:01 -0300

    wip

--- generarExe.bat
+++ generarExe.bat
@@ -2,13 +2,13 @@ c:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c c_files\ts.c c_files\tercetos.c c_files\pila.c -o Primera.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c c_files\ts.c c_files\tercetos.c c_files\pila.c -o Segunda.exe
 pause
 pause
-Primera.exe prueba.txt
+Segunda.exe prueba.txt
 del lex.yy.c
 del y.tab.c
 del y.output
 del y.tab.h
-del Primera.exe
+del Segunda.exe
 pause
